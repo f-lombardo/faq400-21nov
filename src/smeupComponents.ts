40672c0fb15a7ce8da033f8df29a0fc9826d9393
Stefano Macconi <stefano.macconi@gmail.com>
2019-03-08 12:51:02 +0100

    explicity name for production. TODO solve this problem

--- src/smeupComponents.ts
+++ src/smeupComponents.ts
@@ -5,7 +5,7 @@ import Label from '@/components/LAB.vue';
 import Matrix from '@/components/MAT.vue';
 import Field from '@/components/FLD.vue';
 
-Vue.component(Page.name, Page);
-Vue.component(Label.name, Label);
-Vue.component(Matrix.name, Matrix);
-Vue.component(Field.name, Field);
+Vue.component('EXD', Page);
+Vue.component('LAB', Label);
+Vue.component('MAT', Matrix);
+Vue.component('FLD', Field);
