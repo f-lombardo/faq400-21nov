864f19ea074703d8fe473bf269689aa004adfef7
Stefano Macconi <stefano.macconi@gmail.com>
2019-04-16 17:10:32 +0200

    Merged with dynamism-exec and updated mocks

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script lang="ts">
-<<<<<<< HEAD
 import { Component, Vue } from "vue-property-decorator";
 import { mapGetters } from "vuex";
 
@@ -18,18 +17,6 @@ import { mainEXD } from "@/mocks/script";
 })
 export default class Main extends Vue {
   mainComponent: any;
-=======
-import { Component, Vue } from 'vue-property-decorator'
-import { mainEXD } from '@/mocks/script'
-
-@Component
-export default class Main extends Vue {
-
-  get mainComponent() {
-    return this.$store.getters['webup/getMainComponent']
-    // return this.$store.state.webup.root
-  } 
->>>>>>> dynamism-exec
 
   private created() {
     // save root in store
