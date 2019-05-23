9c8d33b946e7c4878f053af68c9e3e5e31ceade6
scrrnd <armando.scarpa@smeup.com>
2019-05-23 12:19:56 +0200

    prove parsing Fun con regex

--- src/classes/Fun.ts
+++ src/classes/Fun.ts
@@ -7,6 +7,22 @@ export default class Fun {
 
   constructor(fun: string) {
     //TODO ci sarà un fun parser o la creo qui dentro?
+    //fun.indexOf("");
+    //https://developer.mozilla.org/it/docs/Web/JavaScript/Guida/Espressioni_Regolari
+    //https://flaviocopes.com/javascript-regular-expressions/
+    /*
+    let expr: RegExp = new RegExp(/F\((.*?)\)/);
+    var result = expr.exec("fun"); //RegExpExecArray
+    console.log(result);
+    if (result) {
+      const matchIndex = result.index;
+      var first: RegExpExecArray = result[0];
+      console.log(result[0]);
+      const firstText = first.toString;
+      const t = first.length;
+      //console.log(new RegRange(matchIndex, t));
+    }
+    */
   }
 
   isServiceInternal(): boolean {
