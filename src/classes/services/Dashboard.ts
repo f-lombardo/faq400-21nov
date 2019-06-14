76bcaa44013be1818816d88077ba7d619a46cbbc
Stefano Macconi <stefano.macconi@gmail.com>
2019-06-14 08:57:13 +0200

    first try with JSON enrichment. Waiting for KetchUP update

--- src/classes/services/Dashboard.ts
+++ src/classes/services/Dashboard.ts
@@ -3,7 +3,42 @@ import Service from "@/classes/Service";
 export default class Dashboard extends Service {
   private path: string = "/gtw-hub/api/services";
 
-  async LIST(): Promise<any> {
+  async DSHLIST(): Promise<any> {
     return this.doGet(this.path + "/dashboard");
+  }
+
+  async MCRSRVLIST(): Promise<any> {
+    var srv = this;
+    return new Promise(function(resolve, reject) {
+      srv.doGet(srv.path + "/microserviceList").then((data: any) => {
+        if (data.rows) {
+          data.rows.forEach((row: any) => {
+            var cellActive = row.cells.ACTIVE;
+            if (cellActive.value == "VO;COD_AGG;000012") {
+              // TODO da mettere solo true
+              cellActive.obj = { t: "J4", p: "ICO" };
+              cellActive.value = "mdi mdi-brightness-1";
+              cellActive.style = { color: "green" };
+            }
+            var cellReady = row.cells.READY;
+            if (cellReady.value == "VO;COD_AGG;000012") {
+              // TODO da mettere solo true
+              cellReady.obj = { t: "J4", p: "ICO" };
+              cellReady.value = "mdi mdi-brightness-1";
+              cellReady.style = { color: "green" };
+            }
+          });
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  async A37LIST(): Promise<any> {
+    return this.doGet(this.path + "/a37MicroserviceList");
+  }
+
+  async A38LIST(): Promise<any> {
+    return this.doGet(this.path + "/a38MicroserviceList");
   }
 }
--- src/mocks/DASH.ts
+++ src/mocks/DASH.ts
@@ -6,24 +6,93 @@ export const DASHBOARD: any = JSON.parse(`
   "fun": "F(EXD;*SCO;) 2(MB;SCP_SCH;[K1])",
   "loaded": true,
   "layout": "column",
-  "sections": [
-    {
+  "sections": [{
       "id": "SDAS1",
-      "components": [
-        {
-          "type": "EXB",
-          "id": "TITDSH",
-          "title": "Dashboard del gateway",
-          "loaded": true,
-          "fun": "F(EXD;DASHBOARD;LIST)",
-          "options": {},
-          "data": {},
-          "messages": [],
-          "actions": {},
-          "variables": [],
-          "dynamisms": []
-        }
-      ]
+      "components": [{
+        "type": "EXD",
+        "id": "SCHDS1",
+        "title": "Scheda Dashboard 1",
+        "fun": "",
+        "loaded": true,
+        "layout": "row",
+        "sections": [{
+            "id": "SDAS1A",
+            "components": [{
+              "type": "EXB",
+              "id": "TITDSH",
+              "title": "Dashboard",
+              "loaded": true,
+              "fun": "F(EXD;DASHBOARD;DSHLIST)",
+              "options": {},
+              "data": {},
+              "messages": [],
+              "actions": {},
+              "variables": [],
+              "dynamisms": []
+            }]
+          },
+          {
+            "id": "SDAS1B",
+            "components": [{
+              "type": "EXB",
+              "id": "TITMSV",
+              "title": "Microservice List",
+              "loaded": true,
+              "fun": "F(EXD;DASHBOARD;MCRSRVLIST)",
+              "options": {},
+              "data": {},
+              "messages": [],
+              "actions": {},
+              "variables": [],
+              "dynamisms": []
+            }]
+          }
+        ]
+      }]
+    },
+    {
+      "id": "SDAS2",
+      "components": [{
+        "type": "EXD",
+        "id": "SCHDS2",
+        "title": "Scheda Dashboard 2",
+        "fun": "",
+        "loaded": true,
+        "layout": "row",
+        "sections": [{
+            "id": "SDAS2A",
+            "components": [{
+              "type": "EXB",
+              "id": "TIT37L",
+              "title": "A37 List",
+              "loaded": true,
+              "fun": "F(EXD;DASHBOARD;A37LIST)",
+              "options": {},
+              "data": {},
+              "messages": [],
+              "actions": {},
+              "variables": [],
+              "dynamisms": []
+            }]
+          },
+          {
+            "id": "SDAS2B",
+            "components": [{
+              "type": "EXB",
+              "id": "TIT38L",
+              "title": "A38 List",
+              "loaded": true,
+              "fun": "F(EXD;DASHBOARD;A38LIST)",
+              "options": {},
+              "data": {},
+              "messages": [],
+              "actions": {},
+              "variables": [],
+              "dynamisms": []
+            }]
+          }
+        ]
+      }]
     }
   ]
 }
