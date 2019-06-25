e4da187d2e52baff2b70ada84f36f476c5e3590f
Francesco Bonacini <f.bonacini@dreamonkey.com>
2019-06-25 13:07:33 +0200

    Updated KetchUP version for real.

--- src/assets/dist/types/components/ketchup-graphic-cell/ketchup-graphic-cell-helper.d.ts
+++
@@ -1,2 +0,0 @@
-import { Color } from './ketchup-graphic-cell-declarations';
-export declare function getColorFromString(rgb: string): Color;
--- src/assets/dist/types/components/ketchup-radio/ketchup-radio-declarations.d.ts
+++
@@ -1,9 +0,0 @@
-export interface KetchupRadioElement {
-    label: string;
-    value: string;
-}
-/**
- * Factory function for KetchupRadioElement
- * @constructor
- */
-export declare function KetchupRadioElementFactory(): KetchupRadioElement;
