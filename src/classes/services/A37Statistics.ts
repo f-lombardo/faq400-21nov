9c85216b8e8437e5bc47373a4364c10cb9fb1f9d
giugianc73 <giuliano.giancristofaro@gmail.com>
2019-09-16 17:27:37 +0200

    Aggiunte statistiche a37

---
+++ src/classes/services/A37Statistics.ts
@@ -0,0 +1,81 @@
+import Service from "@/classes/Service";
+import EnrichUtil from "../utils/EnrichUtil";
+import UIMsg from "../utils/UIMsg";
+
+export default class TemplatesList extends Service {
+  private static PATH: string = "/gtw-hub/api/services";
+
+  async NOWLIST(): Promise<any> {
+    const srv = this;
+    return new Promise(function(resolve, reject) {
+      srv
+        .doGet(TemplatesList.PATH + "/frontend/hub/statisticsListNow")
+        .then((data: any) => {
+          if (data.rows) {
+            data.rows.forEach((row: any) => {
+              let time: Cell = row.cells["TIME"];
+              time = EnrichUtil.addObj(time, "", "", "");
+
+              let incom: Cell = row.cells["IN"];
+              incom = EnrichUtil.addObj(incom, "", "", "");
+
+              let cons: Cell = row.cells["CONS"];
+              cons = EnrichUtil.addObj(cons, "", "", "");
+
+              let out: Cell = row.cells["OUT"];
+              out = EnrichUtil.addObj(out, "", "", "");
+
+              let err: Cell = row.cells["ERR"];
+              err = EnrichUtil.addObj(err, "", "", "");
+
+              let queue: Cell = row.cells["QUEUE"];
+              queue = EnrichUtil.addObj(queue, "", "", "");
+
+              let evtTime: Cell = row.cells["EVTTIME"];
+              evtTime = EnrichUtil.addObj(evtTime, "", "", "");
+
+              return row;
+            });
+          }
+          resolve(data);
+        });
+    });
+  }
+
+  async DATALIST(): Promise<any> {
+    const srv = this;
+    return new Promise(function(resolve, reject) {
+      srv
+        .doGet(TemplatesList.PATH + "/frontend/hub/statisticsList")
+        .then((data: any) => {
+          if (data.rows) {
+            data.rows.forEach((row: any) => {
+              let time: Cell = row.cells["TIME"];
+              time = EnrichUtil.addObj(time, "", "", "");
+
+              let incom: Cell = row.cells["IN"];
+              incom = EnrichUtil.addObj(incom, "", "", "");
+
+              let cons: Cell = row.cells["CONS"];
+              cons = EnrichUtil.addObj(cons, "", "", "");
+
+              let out: Cell = row.cells["OUT"];
+              out = EnrichUtil.addObj(out, "", "", "");
+
+              let err: Cell = row.cells["ERR"];
+              err = EnrichUtil.addObj(err, "", "", "");
+
+              let queue: Cell = row.cells["QUEUE"];
+              queue = EnrichUtil.addObj(queue, "", "", "");
+
+              let evtTime: Cell = row.cells["EVTTIME"];
+              evtTime = EnrichUtil.addObj(evtTime, "", "", "");
+
+              return row;
+            });
+          }
+          resolve(data);
+        });
+    });
+  }
+}
