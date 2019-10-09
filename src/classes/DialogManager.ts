0b3891f1ba222c2b5ded1f098865046c412e833e
giugianc73 <giuliano.giancristofaro@gmail.com>
2019-10-09 16:39:46 +0200

    Revisione NOTIFY

--- src/classes/DialogManager.ts
+++ src/classes/DialogManager.ts
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import store from "../store/store";
 import Message from "./Message";
+import Fun from "./Fun";
 
 export default class DialogManager extends Vue {
-  confirm(message: Message, callback: any): void {
+  confirm(message: Message, fun: Fun): void {
     // https://dzone.com/articles/design-patterns-event-bus
-    const unsubFunc = Vue.prototype.$eventBus.subscribe("dialog", callback);
+    const unsubFunc = Vue.prototype.$eventBus.subscribe("dialog", fun);
+    fun.clearUISetup();
     // console.log("DialogManager->unsubFunc", unsubFunc);
     let main = store.getters["webup/getMain"];
     main.setDialogMessage(message, unsubFunc);
--- src/classes/utils/EventBus.ts
+++ src/classes/utils/EventBus.ts
@@ -1,10 +1,17 @@
+import Fun from "@/classes/Fun";
+import store from "../../store/store";
+import { Vue } from "vue-property-decorator";
+
 export default class EventBus {
   private subscriptions: Array<any> = [];
 
-  public subscribe(eventType: any, callback: any): any {
+  private funSmeup: Fun = new Fun("F(;;)");
+
+  public subscribe(eventType: any, fun: Fun): any {
+    this.funSmeup = fun;
     const id = Symbol("id"); //unique id
     if (!this.subscriptions[eventType]) this.subscriptions[eventType] = {};
-    this.subscriptions[eventType][id] = callback;
+    this.subscriptions[eventType][id] = fun;
     //console.log("EventBus -> subscriptions", this.subscriptions);
     return {
       unsubscribe: function unsubscribe() {
@@ -22,8 +29,26 @@ export default class EventBus {
 
   public publish(eventType: any, arg: any): void {
     if (!this.subscriptions[eventType]) return;
-    Object.getOwnPropertySymbols(this.subscriptions[eventType]).forEach(key =>
-      this.subscriptions[eventType][key](arg)
-    );
+
+    Object.getOwnPropertySymbols(this.subscriptions[eventType]).forEach(key => {
+      var fun = this.subscriptions[eventType][key];
+      console.log("fun", fun);
+      Vue.prototype.$funManager.execute(fun).then(() => {
+        if (this.funSmeup.getNotify()) {
+          var notify = this.funSmeup.getNotify();
+          if (notify) {
+            notify.forEach(function(element) {
+              console.log("Notify element " + element);
+              const notifyVueComponent = store.getters[
+                "webup/getComponentById"
+              ](element);
+              if (notifyVueComponent && notifyVueComponent.hasFun()) {
+                notifyVueComponent.$emit("onExecFun");
+              }
+            });
+          }
+        }
+      });
+    });
   }
 }
