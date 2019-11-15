405ccb426d13c5a198d412f7d9b32fe034a8264a
Stefano Macconi <stefano.macconi@gmail.com>
2019-11-15 17:03:03 +0100

    init

---
+++ src/classes/services/Software.ts
@@ -0,0 +1,15 @@
+import Service from "@/classes/Service";
+
+// TODO REMOVE
+import dataMock from "@/mocks/prova.json";
+
+export default class Dashboard extends Service {
+  private path: string = "/employees?workdept=E21";
+
+  async LIST(): Promise<any> {
+    return new Promise(function(resolve, reject) {
+      //srv.doGet(srv.path).then((data: any) => {
+      resolve(dataMock);
+    });
+  }
+}
