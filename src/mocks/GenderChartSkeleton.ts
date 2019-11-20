25b7cfab2f19d281db063a4b1ba4ae294a25d817
Stefano Macconi <stefano.macconi@gmail.com>
2019-11-20 09:32:54 +0100

    init

---
+++ src/mocks/GenderChartSkeleton.ts
@@ -0,0 +1,74 @@
+export const baseData = {
+  columns: [
+    {
+      name: "Col1",
+      title: "Person"
+    },
+    {
+      name: "Col2",
+      title: "Value1"
+    }
+  ],
+  rows: [
+    {
+      cells: {
+        Col1: {
+          obj: {
+            t: "",
+            p: "",
+            k: "MALE"
+          },
+          value: "MALE"
+        },
+        Col2: {
+          obj: {
+            t: "NR",
+            p: "",
+            k: "0"
+          },
+          value: "0"
+        }
+      }
+    },
+    {
+      cells: {
+        Col1: {
+          obj: {
+            t: "",
+            p: "",
+            k: "FEMALE"
+          },
+          value: "FEMALE"
+        },
+        Col2: {
+          obj: {
+            t: "NR",
+            p: "",
+            k: "0"
+          },
+          value: "0"
+        }
+      }
+    },
+    {
+      cells: {
+        Col1: {
+          obj: {
+            t: "",
+            p: "",
+            k: "OTHER"
+          },
+          value: "OTHER"
+        },
+        Col2: {
+          obj: {
+            t: "NR",
+            p: "",
+            k: "0"
+          },
+          value: "0"
+        }
+      }
+    }
+  ]
+};
